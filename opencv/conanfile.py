35cd9f08b42b83dae56ea022926e75262db9322f
janetatomas11 <janatatomas11@gmail.com>
2020-11-29 13:39:49 +0100

    Basic conanfile for opencv

---
+++ opencv/conanfile.py
@@ -0,0 +1,37 @@
+from conans import ConanFile, CMake, tools
+import os
+
+
+class OpenCVConan(ConanFile):
+    name = "opencv"
+    version = "4.5.0"
+    license = "BSD-3-Clause"
+    homepage = "https://github.com/opencv/opencv"
+    author = "Conan Community"
+    topics = ("opencv", "computer-vision",
+              "image-processing", "deep-learning")
+    extension = "tar.gz"
+    options = {"with_cuda" : [True, False]}
+    default_options = {"with_cuda": True}
+
+    def source(self):
+        tools.download(F"{self.homepage}/archive/{self.version}.{self.extension}",
+                       F"{self.name}-{self.version}.{self.extension}")
+        tools.untargz(F"{self.name}-{self.version}.{self.extension}")
+
+    def _configure_cmake(self):
+        cmake = CMake(self)
+        cmake.verbose = True
+        cmake.configure(source_dir=F"../{self.name}-{self.version}", build_dir="build")
+        return cmake
+
+    def build(self):
+        cmake = self._configure_cmake()
+        cmake.build(args = ["-j8"])
+
+    def package(self):
+        cmake = self._configure_cmake()
+        cmake.install()
+
+    def package_info(self):
+        pass
